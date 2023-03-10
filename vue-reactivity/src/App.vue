61be58d6db95d171806361313c67ec0c28b3577e
pinrongc <pinrongc156@gmail.com>
2023-03-10 14:00:23 -0500

    .

--- vue-reactivity/src/App.vue
+++ vue-reactivity/src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png"> 
-  <h1>{{ HelloWorld }}</h1>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HomePage msg="Welcome to Your Vue.js App"/>
+  <h1>{{ products }}</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import HomePage from './components/main.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    HomePage
   }
 }
 </script>
--- vue-reactivity/src/components/main.vue
+++ vue-reactivity/src/components/main.vue
@@ -1,19 +1,27 @@
 <template>
-    <div class="home">
-<h1>{{products }}</h1>
+ 
+<h1 v-if="buy">{{title}}</h1>
+<h1 v-else>"Did not buy"</h1>
+<!-- <ul>
+  <li  v-for="product in products" :key="product">
+    {{ product }}
 
-    </div>
+  </li> -->
+<!-- </ul> -->
+
+
 </template>
   
   <script>
   
   export default {
-    name: "home",
+    name: 'home-page',
     components: {},
     data() {
         return{
-   products: "apple",
-
+   title: "product",
+/*    products: ["apple", "banana", "orange"], */
+buy: false, 
         };
      
     }
